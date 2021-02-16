3b87524654d777af4ea7223a111c7d97536e08a2
User <User@DESKTOP-K1Q004K>
2021-02-16 10:53:43 +0500

    First commit

---
+++ src/git_project_java/Test1.java
@@ -0,0 +1,19 @@
+package git_project_java;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+			
+		String hungry = "yes";
+		
+		if (hungry=="yes") {
+			
+			System.out.println("It something! ...");
+					}
+		else {			
+			System.out.println("Do work! ...");
+		}
+
+	}
+
+}
